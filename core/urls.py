d9423eb8709e54680bf15bbb8ff9c4d7a8daad29
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-07-08 15:47:00 +0300

    Add / in the end of urls

--- core/urls.py
+++ core/urls.py
@@ -9,21 +9,19 @@ from core.views import (
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
-    task_mark_completed,
-    task_mark_uncompleted
+    ToggleTaskCompletionView
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
-    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
-    path("tasks/<int:pk>/mark-completed", task_mark_completed, name="task-mark-completed"),
-    path("tasks/<int:pk>/mark-uncompleted", task_mark_uncompleted, name="task-mark-uncompleted")
+    path("tasks/<int:pk>/<str:action>/", ToggleTaskCompletionView.as_view(), name="task-toggle-completion"),
 ]
 
 app_name = "core"
