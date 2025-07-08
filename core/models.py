d85efb72204b639eced5e16f4610fca760001af8
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-07-08 13:28:44 +0300

    Implement Tag and Task models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    tag = models.ManyToManyField(
+        to=Tag,
+        related_name="tasks"
+    )
+
+    def __str__(self) -> str:
+        return self.content
